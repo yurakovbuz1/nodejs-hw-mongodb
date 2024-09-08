56520aaa7333e8ce5b882d0b88d7b1df44769bb8
yurakovbuz1 <yura.kovbuz1@gmail.com>
2024-09-08 22:15:31 +0200

    email validation removed

--- src/validation/contacts.js
+++ src/validation/contacts.js
@@ -3,7 +3,7 @@ import Joi from 'joi';
 export const contactsSchema = Joi.object({
   name: Joi.string().min(3).max(20).required(),
   phoneNumber: Joi.string().min(3).max(20).required(),
-  email: Joi.string().min(3).max(20),
+  email: Joi.string(),
   isFavourite: Joi.boolean().required(),
   contactType: Joi.string()
     .valid('work', 'home', 'personal')
