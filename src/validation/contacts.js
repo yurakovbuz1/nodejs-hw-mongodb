7ece5d5b5d85733c252c94798c34011ea342c0c3
yurakovbuz1 <yura.kovbuz1@gmail.com>
2024-09-08 22:45:18 +0200

    reuired field removed from patch

--- src/validation/contacts.js
+++ src/validation/contacts.js
@@ -11,3 +11,11 @@ export const contactsSchema = Joi.object({
     .max(20)
     .required(),
 });
+
+export const patchSchema = Joi.object({
+  name: Joi.string().min(3).max(20),
+  phoneNumber: Joi.string().min(3).max(20),
+  email: Joi.string(),
+  isFavourite: Joi.boolean(),
+  contactType: Joi.string().valid('work', 'home', 'personal').min(3).max(20),
+});
